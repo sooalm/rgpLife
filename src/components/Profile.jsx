4fea7b580d4dab10695c5e923f87681a8fcb0484
Eugene <sshabrovivivi@gmail.com>
2025-08-08 18:16:23 +0400

    Initial commit of project files

---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="card">
+      <img className="card__img"></img>
+      <h2>@sooalm</h2>
+      <p>Administraitor</p>
+      <div className="card_stats"></div>
+    </div>
+  );
+};
+
+export default Profile;
