9fc13fd5b607a4b6db26c4db6b94c98c7b7577f1
Eugene <sshabrovivivi@gmail.com>
2025-09-19 13:19:14 +0400

    junior mind virtualization

--- src/components/pages/Achievements/Achievements.jsx
+++ src/components/pages/Achievements/Achievements.jsx
@@ -1,43 +1,22 @@
-import React, { useState, useRef, useEffect,useCallback} from "react";
+import React, { useState, useRef, useEffect,useCallback,memo,useMemo} from "react";
 import { v4 as uuidv4 } from "uuid";
 
 import "/src/styles/Achievements.css";
 import trophy from "/src/assets/trophy.svg";
 
-const Achievements = () => {
-  const [startVisibleItems, setStartVisibleItems] = useState(0);
-  const [visibleItems, setVisibleItems] = useState([]);
-  const items = useRef([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [spacer, setSpacer] = useState(0);
-
-  const paginationPage = useRef(1);
-
-  const containerRef = useRef(null);
-  const scrollref = useRef(null);
-  const numItemsPerRow = useRef(0);
-  const numVisibleRows = useRef(4);
-  const itemHeight = useRef(0);
-  const itemWidth = useRef(0);
-
-  const start = useRef(0);
-  const end = useRef(42);
-
-  async function fetchDataIncrementally(page=1 , limit = 20) {
+  async function fetchData(page = 1, limit = 40) {
     console.log("Fetching page:", page);
-
+    // if(page==1) limit =40;
     try {
       const response = await fetch("/src/assets/data/achievements.json");
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
       const data = await response.json();
-
       // Пагинация: вычисляем начальный и конечный индексы
       const startIndex = (page - 1) * limit;
       const endIndex = startIndex + limit;
 
-      
       const paginatedData = data.slice(startIndex, endIndex);
 
       return paginatedData;
@@ -46,162 +25,169 @@ const Achievements = () => {
       return [];
     }
   }
-  function infinityScroll() {
-    // if(items.current.length==0) setTop(1);
-    if (isLoading) return; // Prevent multiple simultaneous fetches
-    setIsLoading(true);
-
-    const currentPage = paginationPage.current;
-    fetchDataIncrementally(currentPage).then((newItems) => {
-     if (newItems.length === 0) {
-      console.log('No more items to load');
-      return;
-       }
-       newItems.forEach(item => {
-      if( !items.current.some(existingItem => existingItem.id === item.id)){
-        paginationPage.current = currentPage + 1;
-        setIsLoading(false);
-        items.current = [...items.current, ...newItems];
-     
-      }else setIsLoading(false);
-    });
-    }).catch(error => {
-      console.error('Error loading items:', error);
-      setIsLoading(false);
-    });
+  
+function InfiniteWrapper({children,fetchData,handleFetchData,paginationPage,virtualizerRef,scrollref,setItems,setIsLoading,items,itemHeight,startIndex,numberPerLine,visibleItems}){
+      virtualizerRef.current =  useCallback(
+          (event) => {
+            if (event.target.scrollTop >= (visibleItems.length/numberPerLine.current) * itemHeight ) { // calculate when to fetch new data
+             handleFetchData();
+            //  console.log("работаем")
+            }
+          },
+          [startIndex, setItems] //setIsFinished
+        );
+  return <>{children}</>;
 }
-  // Throttle the virtualizer function to reduce recalculations
-  const virtualizer = useCallback(() => {
-    if (!scrollref.current) return;
-    
-    const scroll = scrollref.current;
-    
-      const height = itemHeight.current || 180;
-      const width = itemWidth.current || 170;
-      
-      numItemsPerRow.current = Math.max(1, Math.round(scroll.clientWidth / width));
-      numVisibleRows.current = Math.ceil(scroll.clientHeight / height); // Add extra row for smoother scrolling
-      
-      const scrollTop = scroll.scrollTop;
-      // setTop(prev => prev !== scrollTop ? scrollTop : prev);
-      
-      
-      const totalVisibleItems = numItemsPerRow.current * (numVisibleRows.current + 1); // Add buffer for smooth scrolling
-      
-      const totalRows = Math.ceil(items.current.length / numItemsPerRow.current);
-      const maxScroll = totalRows * height - scroll.clientHeight;
-      
-      // Calculate start index based on scroll position
-      //костыльный if который не дает ререндерить когда мы в самомо неизу и тем самым исправляет этот дерганый баг
-      // if(start.current >  Math.max(Math.floor(scrollTop / height) * numItemsPerRow.current) && end.current== Math.min(items.current.length, start.current+totalVisibleItems+start.current)){
-        
-      
-     
-        
-        start.current = Math.floor(scrollTop / height) * numItemsPerRow.current ;
-      
-      // Calculate end index, ensuring we don't go past the end of the array
-      
-       end.current =  Math.min(items.current.length, start.current+totalVisibleItems+start.current);
-        setSpacer(prev=>Math.floor(scrollTop- start.current*height ));
-    // }
-      console.log(items.current.length,scrollTop,spacer,"start",start.current,"end",end.current);
-
-      const data = items.current.slice(start.current, end.current);
-      setVisibleItems(data);
-      setStartVisibleItems(prev=>prev!=start.current?start.current:prev);
-      
-    
-  })
-
-  useEffect(() => {
-  //  console.log("virt: ",items.current.length)
-      
-      requestAnimationFrame(() => {
-        virtualizer();
-      });
-    // console.log("Items updated, length:", items.length,visibleItems);
-  }, [isLoading]);
-
-  // Initial data load
-  useEffect(() => {
+function VirtualWrapper({children,itemHeight,scrollref,setStartIndex,virtualizerRef,numberPerLine}){
+  
+  const perLIne = numberPerLine.current;
+  // console.log(perLIne,numberPerLine.current)
+  const handleVirtualScroll = useCallback((event) => {
    
-    if (items.current.length === 0) {
-      console.log('Initial data loading...');
-  
-      requestAnimationFrame(() => {
-        infinityScroll();
+    const newStartIndex = Math.floor(event.target.scrollTop / itemHeight);
+    setStartIndex(newStartIndex);
+    virtualizerRef.current(event); //Initialize infinity scroll wrapper
+
+   
+  }, [setStartIndex,itemHeight,perLIne]);
+
+  useEffect(() => {
+    const scrollElement = scrollref.current;
+    if (!scrollElement) return;
+    
+    scrollElement.addEventListener("scroll", handleVirtualScroll);
+    return () => {
+      scrollElement.removeEventListener("scroll", handleVirtualScroll);
+    };
+  }, [handleVirtualScroll]);
+  return <>{children}</>;
+}
+
+
+function VirtualisedInfiniteList(){
+
+  const virtualizerRef = useRef(null);
+  // const handleFetchData = useRef(null);
+  const scrollref = useRef(null);
+  const paginationPage = useRef(1);
+
+  // const [isLoading, setIsLoading] =useState(false);
+  const isLoading = useRef(false);
+
+  const [startIndex, setStartIndex] =useState(0);
+  const [items, setItems] = useState([]);
+
+
+
+  const handleFetchData = useCallback(() => {
+    if(isLoading.current) return;
+    
+   
+    const currentPage = paginationPage.current; // Сохраняем текущую страницу
+    // console.log(`Начинаем загрузку страницы: ${currentPage}`);
+    isLoading.current=true;
+
+    fetchData(currentPage).then((newItems) => {
+      if (!newItems.length) {
+        console.log("No more data");
+        isLoading.current=false;
+        return;
+      }
+      setItems(prevItems => {
+        const uniqueNewItems = newItems.filter(newItem => 
+          !prevItems.some(item => item.id === newItem.id)
+        );
+      if (uniqueNewItems.length > 0) {
+        paginationPage.current =currentPage+ 1;
+        // console.log(`Успешно загружена страница: ${currentPage}, следующая: ${paginationPage.current}`);
+      }
+      isLoading.current=false;
+      return [...prevItems, ...uniqueNewItems];
       });
      
-    }
-    const handleScroll = () => {
-      const { scrollTop, scrollHeight, clientHeight } = scrollref.current;
-      // Load more when scrolled to 70% of the container
-      const scrollThreshold = 0.7;
-      requestAnimationFrame(virtualizer);
-      if (scrollTop + clientHeight >= scrollHeight * scrollThreshold 
-       ) {
-        requestAnimationFrame(() => {
-          infinityScroll();
-          // virtualizer();
-        });
-      }
-    };
-  
-    const scroll = scrollref.current;
-    if (scroll) {
-      // Using passive: true for better performance
-      scroll.addEventListener('scroll', handleScroll, { passive: true });
-      return () => {
-        scroll.removeEventListener('scroll', handleScroll);
-      };
-    }
-  }, []);
- 
+      }).catch(error => {
+        console.error("Ошибка при загрузке данных:", error);
+        isLoading.current = false;
+      });
+    },[]);
 
-  const handleFirstItemRef = (node) => {
-    if (node !== null) {
-      const { height,width } = node.getBoundingClientRect();
-      itemHeight.current = height;
-      itemWidth.current = width;  
-    }
-  };
-  
+    const firstItem = useRef(null);
+    const itemHeight = firstItem.current ? firstItem.current.offsetHeight : 175;
+    const itemWidth = firstItem.current ? firstItem.current.offsetWidth : 165;;
+
+    const numberPerLine = useRef(5);
+
+      const visibleItems = useMemo(() => {
+      
+        return items.slice(Math.floor(startIndex*numberPerLine.current), Math.floor(startIndex*numberPerLine.current) + 5*numberPerLine.current);
+      }, [startIndex, items]);
+
+    useEffect(() => {
+      numberPerLine.current = Math.ceil(scrollref.current.clientWidth / itemWidth);
+      handleFetchData();
+    }, []);
+
+    
+return(
+  <>  
+  <InfiniteWrapper 
+      handleFetchData={handleFetchData}
+      items={items}  setItems={setItems} 
+      paginationPage={paginationPage}
+   
+      scrollref={scrollref}
+      
+      virtualizerRef={virtualizerRef}
+      itemHeight={itemHeight}
+      numberPerLine={numberPerLine}
+      visibleItems={visibleItems}
+      startIndex={startIndex}
+      >
+    <VirtualWrapper 
+        itemHeight={itemHeight} 
+        scrollref={scrollref} 
+        setStartIndex={setStartIndex}
+        virtualizerRef={virtualizerRef}
+        numberPerLine={numberPerLine}
+        >
+      <List
+          items = {visibleItems}
+          itemHeight={itemHeight}
+          scrollref={scrollref}
+          startIndex={startIndex}
+          numberPerLine={numberPerLine}
+          firstItem= {firstItem}
+          numItems={items.length}
+      />
+    </VirtualWrapper>
+  </InfiniteWrapper>
+  </>
+);
+}
+// style={{ height: `${(startIndex + 20)/numberPerLine * itemHeight}px` }}>
+function List({items,itemHeight,scrollref,startIndex,numberPerLine,firstItem,numItems}) {
+
   return (
     <div className="achievments" ref={scrollref}>
-      <div
-        className="spacer"
-        style={{
-          // height: `${Math.floor(startVisibleItems/numItemsPerRow.current)* (itemHeight.current || 170)}px`,
-          height: `${spacer}px`,
-          width: "100%",
-          pointerEvents: "none",
-        }}
-      ></div>
-      <div className="achievements-list" ref={containerRef}>
-        {visibleItems.map((item, index) => (
-          <Achievement 
-            key={uuidv4()} 
-            item={item} 
-            ref={index === 0 ? handleFirstItemRef : null} 
-          />
-        ))}
+      <div   style={{
+            height: `${(Math.ceil(items.length/numberPerLine.current))* itemHeight}px`
+          }}>
+        <div className="achievements-list" style={{
+            position: "relative",
+            top: `${Math.floor(startIndex) * itemHeight}px`, }}>
+          {items.map((item,i) => (
+            <ListItem 
+             key={uuidv4()} 
+             item={item}
+             ref={i===0 ? firstItem : null}
+           />
+          ))}
+        </div>
       </div>
-      <div
-        className="spacerBot"
-        style={{
-          height: `${10}px`,
-          width: "100%",
-          pointerEvents: "none",
-          visibility: "hidden"
-        }}
-      ></div>
     </div>
   );
 };
-
-const Achievement = React.forwardRef(({ item }, ref) => {
-  
+const ListItem = memo(({ item }) => {
   let rarityColor = 0;
   switch (item.rarity) {
     case "common":
@@ -230,7 +216,7 @@ const Achievement = React.forwardRef(({ item }, ref) => {
       <img src={trophy} className="achievements-list__trophy " />
       <div className="item" >
         <p className="item__name" style={{ color: "black" }}>
-          {item.name } 
+          {item.name} 
         </p>
         <p
           className="item__description"
@@ -244,6 +230,7 @@ const Achievement = React.forwardRef(({ item }, ref) => {
       </div>
     </div>
   );
-  
 });
-export default Achievements;
+
+export default VirtualisedInfiniteList;
+
